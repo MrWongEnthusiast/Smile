b8e0e8eefebeb4acdccf85d2d4a8c690d0f9df58
MrWongEnthusiast <100166774+MrWongEnthusiast@users.noreply.github.com>
2022-03-01 10:45:21 +1300

    Andrew fixed define error, program now runs on a development server

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-from flask import flask
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'The Official Mr Stevenson Fan Page'
+
+
+app.run(host='0.0.0.0')
