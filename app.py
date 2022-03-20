68db0db8010898b5455ec93c9b8f28696c9f3e2b
MrWongEnthusiast <100166774+MrWongEnthusiast@users.noreply.github.com>
2022-03-21 12:24:10 +1300

    Added home, contact, menu, and smile.css

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'The Official Mr Stevenson Fan Page'
+def render_menu_page():
 
+     render_template('menu.html')
 
 app.run(host='0.0.0.0')
