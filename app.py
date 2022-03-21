a62c12bfe3039101c362fa4d88684b65e2138620
MrWongEnthusiast <100166774+MrWongEnthusiast@users.noreply.github.com>
2022-03-22 10:47:38 +1300

    Fixed internal server error upon accessing development server

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
-@app.route('/')
-def render_menu_page():
 
-     render_template('menu.html')
+@app.route('/')
+def render_home_page():
+    return render_template('home.html')
+@app.route('/menu')
+def render_menu_page():
+    return render_template('menu.html')
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
+
+
 
 app.run(host='0.0.0.0')
