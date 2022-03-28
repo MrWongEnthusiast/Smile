9d29205541d886552778607aa8397ff2f122155e
MrWongEnthusiast <100166774+MrWongEnthusiast@users.noreply.github.com>
2022-03-29 11:10:39 +1300

    Added quick input for new products with sqlite.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, session
 import sqlite3
 from sqlite3 import Error
-DB_NAME = "smile.db"
+
+DB_NAME = "USC.db"
 
 app = Flask(__name__)
 
@@ -21,14 +22,14 @@ def render_home_page():
 
 @app.route('/menu')
 def render_menu_page():
-
     con = create_connection(DB_NAME)
 
     query = "SELECT name, description, volume, price, image FROM product"
 
-    product_list = [["Flat White","Flat white reminds me of my ex *badoomch* i want you back please text back please","180ml", "flatwhite", "3.00"],
-                    ["Chemex","It sounds like something putin would you on populated civilian areas.","200ml","chemex","4.00"],
-                    ["Aeropress","Aeropress, just call it a plunger you muppet.","180ml","aeropress","2.00"]]
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
 
     return render_template('menu.html', products=product_list)
 
@@ -39,3 +40,5 @@ def render_contact_page():
 
 
 app.run(host='0.0.0.0')
+
+# http://10.50.16.37:5000/
